ad534a2d2707e7a44a5fa08d91ca74c25b31bf02
Mrinal Mayank <65720872+mrinalmayank7@users.noreply.github.com>
2026-06-23 14:07:58 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,40 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack=[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minstack:
+            self.minstack.append(val)
+        else:
+            self.minstack.append((min(val, self.minstack[-1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if not self.minstack:             
+            return -1
+        return self.minstack[-1]
+        #brute force
+        #return min(self.stack)
+        
+
+
+"""
+keep a miminum stack, of same size actual stack. 
+when pushed at current state of stack its min is stored.
+when popped current min is also popped
+
+
+optimal:
+getMin : o(1) with minstack
+directly finding min is o(n)
+"""
