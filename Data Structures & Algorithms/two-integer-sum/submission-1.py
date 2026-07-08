ef2488bc8a339a6db6bd0c60432b85ba32e48ca5
Mrinal Mayank <65720872+mrinalmayank7@users.noreply.github.com>
2026-07-08 16:02:59 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      hashmap={}
+      for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap:
+          return[hashmap[complement],i]
+        else:
+          hashmap[nums[i]]= i
+      return []
+        
+    def twoSumBrute(self, nums: List[int], target: int) -> List[int]:
+      for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+          if nums[i] + nums[j] == target:
+            return [i,j]
+      return []
